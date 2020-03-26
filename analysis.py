b5baa3c31f4847c890dbc071bf170767ad54b1a7
ashleycampion <ashleydcampion@gmail.com>
2020-03-26 20:40:15 +0000

    first commit for histogram file

---
+++ analysis.py
@@ -0,0 +1,14 @@
+#from createSummaries import createSummaries
+from createHistograms import createHistograms
+#from createScatterPlots import createScatterPlots
+
+
+def main():
+    #createSummaries()
+    createHistograms()
+    #createScatterPlots()
+
+
+
+if __name__ == '__main__':
+        main()
