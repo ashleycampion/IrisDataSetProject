a706138965dd63f6aa09e8a0db7cd3daea5c51b5
ashleycampion <ashleydcampion@gmail.com>
2020-03-26 23:30:16 +0000

    adding function to find indexes for species

--- createDataFrame.py
+++ createDataFrame.py
@@ -1,3 +1,20 @@
 import pandas as pd
 
 df = pd.read_csv("irisDataSet.txt")
+
+versicolor = 0
+virginica = 0
+
+def findIndexesForSlicing(df):
+    global versicolor
+    global virginica
+    for i in range(len(df)):
+        if df[i] =='versicolor':
+            versicolor = i
+            break
+    for i in range(len(df)):
+        if df[i] == 'virginica':
+            virginica = i
+            break
+
+findIndexesForSlicing(df["species"])
