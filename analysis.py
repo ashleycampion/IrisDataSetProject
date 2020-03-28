87acff410364ffd415e3d4cee126e4381f66d84d
ashleycampion <ashleydcampion@gmail.com>
2020-03-28 17:19:25 +0000

    adding comments and uncommenting createSummaries call

--- analysis.py
+++ analysis.py
@@ -1,15 +1,18 @@
+# While the other files in the repository can be run
+# on their own, by running this file one can run them
+# all from a central location.
+
 from verifyIntegrityOfDataSet import verifyIntegrityOfDataSet
-# from createSummaries import createSummaries
+from createSummaries import createSummaries
 from createHistograms import createHistograms
 from createScatterPlots import createScatterPlots
 
 
 def main():
     verifyIntegrityOfDataSet()
-    # createSummaries()
+    createSummaries()
     createHistograms()
     createScatterPlots()
-
 
 
 if __name__ == '__main__':
