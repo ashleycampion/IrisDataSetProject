c06dcac4cae236d83e42a5d5cf8d4b953e438d5b
ashleycampion <ashleydcampion@gmail.com>
2020-04-07 18:48:15 +0100

    using os model to create directory to save plots

--- createBoxplots.py
+++ createBoxplots.py
@@ -10,7 +10,35 @@ import seaborn as sb
 # Swarm plot is particularly effective when overlayed
 # on a boxplot.
 
-def createBoxplots():
+# we will use the os module to create the
+# the directory to store the files to
+import os
+
+def createBoxPlots():
+
+    # I adapted the code for creating a directory
+    # if it doesn't already exist from here:
+    # https://stackoverflow.com/questions/11373610/save-matplotlib-file-to-a-directory
+    # first we get the absolute path of the current directory
+    # by calling the dirname() method on the os module's path
+    # field with a parameter of the current filename
+    # (represented by the 'dunder' or 'magic' __file__
+    # variable that is built into Python).
+    scriptDir = os.path.dirname(__file__)
+    # we then create the path of the new directory by calling
+    # the join() method on the os module's path field with two
+    # parameter, the first being the current directory's path, and the
+    # second being the subdirectory we want to add to it.
+    plotDir = os.path.join(scriptDir, 'plots/boxPlots/')
+    # we then check if the directory we want to create already
+    # exists, and only if it doesn't exist do we invoke the os
+    # module's makedirs() method to create it.
+    if not os.path.isdir(plotDir):
+        os.makedirs(plotDir)
+
+
+
+
     # because only an axes object can call the set_title method,
     # we assign the boxplot to a variable, and then use
     # that to call the set_title method
@@ -21,7 +49,7 @@ def createBoxplots():
     plot = sb.swarmplot(y="sepal_length", x="species", data=df, color="black", alpha=0.75)
     # again, seaborn is not as user-friendly as pyplot, and
     # we must use the plot's 'figure' object to call savefig()
-    plot.figure.savefig("plots/boxplots/sepalLength.png")
+    plot.figure.savefig(plotDir + "sepalLength.png")
     # apparently, pyplot should be used to close seaborn plots:
     # https://stackoverflow.com/questions/57533954/how-to-close-seaborn-plots
     plt.close()
@@ -29,19 +57,19 @@ def createBoxplots():
     plot = sb.boxplot(y="sepal_width", x="species", data=df, width=0.5)
     plot.set_title("Species vs. Sepal Width")
     plot = sb.swarmplot(y="sepal_width", x="species", data=df, color="black", alpha=0.75)
-    plot.figure.savefig("plots/boxplots/sepalWidth.png")
+    plot.figure.savefig(plotDir + "sepalWidth.png")
     plt.close()
 
     plot = sb.boxplot(y="petal_length", x="species", data=df, width=0.5)
     plot.set_title("Species vs. Petal Length")
     plot = sb.swarmplot(y="petal_length", x="species", data=df, color="black", alpha=0.75)
-    plot.figure.savefig("plots/boxplots/petalLength.png")
+    plot.figure.savefig(plotDir + "petalLength.png")
     plt.close()
 
     plot = sb.boxplot(y="petal_width", x="species", data=df, width=0.5)
     plot.set_title("Species vs. Petal Width")
     plot = sb.swarmplot(y="petal_width", x="species", data=df, color="black", alpha=0.75)
-    plot.figure.savefig("plots/boxplots/petalWidth.png")
+    plot.figure.savefig(plotDir + "petalWidth.png")
     plt.close()
 
     # as thee setosa petal length and width are closely clustered,
@@ -60,4 +88,4 @@ def createBoxplots():
 # someone would expect to happen should they choose
 # to run this rile on its own
 if __name__ == '__main__':
-    createBoxplots()
+    createBoxPlots()
